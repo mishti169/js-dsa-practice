ec3f8226a92d3ca8e0c395134d97b378db6ff6a5
mishti169 <91414075+mishti169@users.noreply.github.com>
2023-06-05 11:54:44 +0530

    2 files modified, 1 added

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 001 ---> Array
 001.js -> linear search
 002.js-> reverseAnArr
+003.js-> reverseAstr
--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS practice</h1>`;
 import './001/001.js';
 import './001/002.js';
+import './001/003.js';
