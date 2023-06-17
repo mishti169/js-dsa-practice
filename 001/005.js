e9246b9aeffb162c0cc50e19dea682f7cd450728
mishti169 <91414075+mishti169@users.noreply.github.com>
2023-06-17 09:46:44 +0530

    2 files modified

--- 001/005.js
+++ 001/005.js
@@ -1,11 +1,25 @@
 const arr = [3, 5, 1, 6, 9, 4];
-const n = 5;
-const unsortedArr = (arr, n) => {
+const n = 6;
+
+//SEARCH METHOD
+const searchInUnsortedArr = (arr, n) => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === n) {
       return i;
     }
   }
+  return -1;
 };
+// console.log(searchInUnsortedArr(arr, n));
 
-unsortedArr(arr, n);
+// INSERT METHOD
+
+// insert at the end
+const insertAtTheEnd = (arr, n) => {
+  arr.push(n);
+};
+insertAtTheEnd(arr, n);
+console.log(arr);
+
+//insert at the middle
+const insertATTheMiddle = () => {};
