f869ee56e519201ba55cfcb379efca934edf5eae
mishti169 <91414075+mishti169@users.noreply.github.com>
2023-06-19 10:39:53 +0530

    1 file modified

--- 001/007.js
+++ 001/007.js
@@ -51,7 +51,32 @@ const binarySearch2 = (array, key1st, low, high) => {
     return binarySearch2(array, key1st, low, mid - 1);
   }
 };
-const i = binarySearch2(array, key1st, 0, array.length - 1);
-console.log(i);
+// const i = binarySearch2(array, key1st, 0, array.length - 1);
+// console.log(i);
 
-//last
+//last occurance
+
+const array2 = [1, 2, 3, 3, 3, 3, 4, 5, 6, 7, 8, 9];
+const keyLast = 10;
+
+const binarySearch3 = (array, keyLast, low, high) => {
+  const mid = Math.floor((low + high) / 2);
+
+  if (high < low) {
+    return -1;
+  }
+
+  if (array[mid] === keyLast) {
+    let next = mid + 1;
+    while (keyLast === array[next]) {
+      next = next + 1;
+    }
+    return next - 1;
+  } else if (keyLast > array[mid]) {
+    return binarySearch3(array, keyLast, mid + 1, high);
+  } else {
+    return binarySearch3(array, keyLast, low, mid - 1);
+  }
+};
+const i = binarySearch3(array2, keyLast, 0, array.length - 1);
+console.log(i);
