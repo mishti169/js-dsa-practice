1b953161e2e1fb0fb64be90b70e9a739116783d3
mishti169 <91414075+mishti169@users.noreply.github.com>
2023-06-19 12:02:57 +0530

    1 file modified

--- 001/008.js
+++ 001/008.js
@@ -1,18 +1,22 @@
 // search , insert and delet in sorted array
-//**************************************************************************************
 const arr1 = [1, 2, 3, 4, 5, 7, 8, 9];
 const insertVal = 6;
 
+//**************************************************************************************
 //insert in sorted arr
 
 const insertInSorted = (arr1, insertVal) => {
   let i;
   for (i = 0; i < arr1.length; i++) {
-    if (arr[i] >= insertVal) {
+    if (arr1[i] >= insertVal) {
       arr1.splice(i, 0, insertVal);
+      return;
     }
   }
 };
 
-// console.log(insertInSorted(arr1, insertVal));
 // insertInSorted(arr1, insertVal);
+// console.log(arr1);
+
+//******************************************************************************************
+// Delet in sorted
