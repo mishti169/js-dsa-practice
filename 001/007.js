b93bebc396aa9f8c3bfe4ddc5c16f5daeec695fc
mishti169 <91414075+mishti169@users.noreply.github.com>
2023-06-19 10:35:31 +0530

    1 file modified

--- 001/007.js
+++ 001/007.js
@@ -32,7 +32,8 @@ const array = [1, 2, 3, 3, 3, 3, 4, 5, 6, 7, 8, 9];
 const key1st = 3;
 
 const binarySearch2 = (array, key1st, low, high) => {
-  const mid = Math.floor(low + high / 2);
+  // debugger;
+  const mid = Math.floor((low + high) / 2);
 
   if (high < low) {
     return -1;
@@ -45,10 +46,12 @@ const binarySearch2 = (array, key1st, low, high) => {
     }
     return prev + 1;
   } else if (key1st > array[mid]) {
-    binarySearch2(array, key1st, mid + 1, high);
+    return binarySearch2(array, key1st, mid + 1, high);
   } else {
-    binarySearch2(array, key1st, low, mid - 1);
+    return binarySearch2(array, key1st, low, mid - 1);
   }
 };
-// const i = binarySearch2(array, key1st, 0, array.length - 1);
-// console.log(i);
+const i = binarySearch2(array, key1st, 0, array.length - 1);
+console.log(i);
+
+//last
