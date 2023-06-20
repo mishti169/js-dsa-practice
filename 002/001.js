62e870f5c6cf7c522ffffe85c1ba143a2bbc3b01
mishti169 <91414075+mishti169@users.noreply.github.com>
2023-06-20 12:13:36 +0530

    1 file modified

--- 002/001.js
+++ 002/001.js
@@ -20,14 +20,36 @@ class LinkedList {
       this.tail = node;
       return;
     }
-
+    // adds another node
     this.tail.next = node;
     this.tail = node;
   }
 
+  pop() {
+    let temp = this.head;
+    if (this.head === null) {
+      throw Error('first go and get some food then poop');
+    }
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+      return;
+    }
+
+    while (temp.next.next !== null) {
+      temp = temp.next;
+    }
+    temp.next = null;
+    this.tail = temp;
+  }
+
   print() {
     console.log('hiii i am print ');
+    if (this.head === null) {
+      throw Error('first go and get some food then print');
+    }
     let temp = this.head;
+    //jump till temp is null
     while (temp !== null) {
       console.log(temp.val);
       temp = temp.next;
@@ -35,8 +57,11 @@ class LinkedList {
   }
 }
 
-// const ll = new LinkedList();
+const ll = new LinkedList();
 // ll.push(10);
 // ll.push(20);
 // ll.push(30);
-// ll.print();
+// ll.pop();
+// ll.pop();
+// ll.pop();
+ll.print();
