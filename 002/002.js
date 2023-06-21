1ca6b398f4c57177ba1666e91707bf92f99dc412
mishti169 <91414075+mishti169@users.noreply.github.com>
2023-06-21 12:01:54 +0530

    1 file modified

--- 002/002.js
+++ 002/002.js
@@ -14,4 +14,45 @@ class DoublyLinkedList {
     this.tail = null;
     this.size = 0;
   }
+
+  push(val) {
+    const node = new Node(val);
+
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+      return;
+    }
+
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = node;
+  }
+
+  pop() {
+    debugger;
+    let temp = this.tail.prev;
+    this.tail.prev = null;
+    temp.next = null;
+    this.tail = temp;
+  }
+
+  print() {
+    if (this.head === null) {
+      throw Error('head is already null');
+    }
+
+    let temp = this.head;
+    while (temp != null) {
+      console.log(temp.val);
+      temp = temp.next;
+    }
+  }
 }
+const dll = new DoublyLinkedList();
+dll.push(10);
+dll.push(20);
+dll.push(30);
+dll.push(40);
+dll.pop();
+dll.print();
