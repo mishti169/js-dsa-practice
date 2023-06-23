fd3d3026adacaf2d7db49d0982dbb1737101aa65
mishti169 <91414075+mishti169@users.noreply.github.com>
2023-06-23 11:03:17 +0530

    1 file modified

--- 002-LL/003.js
+++ 002-LL/003.js
@@ -1,1 +1,54 @@
 console.log('circular singly LL');
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+class CircularSinglyLL {
+  constructor(val) {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append(val) {
+    const node = new Node(val);
+    this.size++;
+
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+      this.tail.next = this.head;
+      return;
+    }
+
+    node.next = this.head;
+    this.head = node;
+    this.tail.next = node;
+  }
+
+  push(val) {
+    const node = new Node(val);
+    this.size++;
+
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+      this.tail.next = this.head;
+      return;
+    }
+
+    this.tail.next = node;
+    this.tail = node;
+    this.tail.next = this.head;
+  }
+}
+
+const csll = new CircularSinglyLL();
+csll.append(9);
+csll.append(8);
+// csll.push(10);
+// csll.push(20);
+console.log(csll);
