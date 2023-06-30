ec0f0affd24ff367d8cd5fe42eba09a69600f08d
mishti169 <91414075+mishti169@users.noreply.github.com>
2023-06-30 12:12:52 +0530

    1 file modified

--- 003-SORTING/002.js
+++ 003-SORTING/002.js
@@ -1,6 +1,6 @@
 console.log('i m selection sort');
 
-let arr = [4, 7, 2, 0, 9, 1, -1];
+let arr = [4, 7, -20, 0, 9, 1, -1];
 console.log(arr);
 const swap = (i1, i2) => {
   let temp = arr[i1];
@@ -9,12 +9,12 @@ const swap = (i1, i2) => {
 };
 
 for (let j = 0; j < arr.length; j++) {
-  let smallest = j;
-  for (let i = smallest + 1; i < arr.length; i++) {
-    if (arr[i] < smallest) {
-      smallest = i;
+  let smallestIndex = j;
+  for (let i = smallestIndex + 1; i < arr.length; i++) {
+    if (arr[i] < arr[smallestIndex]) {
+      smallestIndex = i;
     }
   }
-  swap(j, smallest);
+  swap(j, smallestIndex);
 }
 console.log(arr);
