60ca71fe3eaf3b108addc99e4f54832c054d4e58
mishti169 <91414075+mishti169@users.noreply.github.com>
2023-06-30 11:39:19 +0530

    1 file modified

---
+++ 003-SORTING/002.js
@@ -0,0 +1,21 @@
+console.log('i m selection sort');
+
+let arr = [5, 3, 7, 1, 9, 4];
+
+// function swap(i1, i2) {
+//   let temp = arr[i1];
+//   arr[i1] = arr[i2];
+//   arr[i2] = temp;
+// }
+
+let smallest = j;
+
+// for (let j = 0; j < arr.length; j++) {
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < smallest) {
+//       smallest = arr[i];
+//     }
+//   }
+// }
+
+// // swap(arr[i],  smallest);
